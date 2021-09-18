c173b89767178c1b2f4776f77f70dc7111f9557c
wbs <1134954328@qq.com>
2021-09-18 23:44:41 +0800

    解决导航栏点击区域过小的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,22 +1,29 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import Icon from "./Icon";
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+import Icon from './Icon';
 
 const NavWrapper = styled.nav`
   line-height: 22px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
   > ul {
     display: flex;
+
     > li {
       width: 33.33333%;
       text-align: center;
-      padding: 5px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      .icon {
-        width: 24px;
-        height: 24px;
+
+      > a {
+        padding: 5px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        >
+        .icon {
+          width: 24px;
+          height: 24px;
+        }
       }
     }
   }
@@ -27,16 +34,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="tags"/>
-          <Link to="/tabs">标签页</Link>
+          <Link to="/tabs">
+            <Icon name="tags"/>
+            标签页
+          </Link>
         </li>
         <li>
-          <Icon name="money"/>
-          <Link to="/money">记账页</Link>
+          <Link to="/money">
+            <Icon name="money"/>
+            记账页
+          </Link>
         </li>
         <li>
-          <Icon name="statistics"/>
-          <Link to="/statistics">统计页</Link>
+          <Link to="/statistics">
+            <Icon name="statistics"/>
+            统计页
+          </Link>
         </li>
       </ul>
     </NavWrapper>
