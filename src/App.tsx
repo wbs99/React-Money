7cd3979995b9959c8136f8e965ecfc291fc5864b
wbs <1134954328@qq.com>
2021-09-19 20:11:31 +0800

    完成标签模块布局

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,35 @@ import Money from './views/Money';
 import Tabs from './views/Tabs';
 import Statistics from './views/Statistics';
 import NotFound from './views/NotFound';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  color: #333;
+`
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/tabs">
-          <Tabs/>
-        </Route>
-        <Route path="/money">
-          <Money/>
-        </Route>
-        <Route path="/statistics">
-          <Statistics/>
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/money"/>
-        </Route>
-        <Route path="*">
-          <NotFound/>
-        </Route>
-      </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route path="/tabs">
+            <Tabs/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/money"/>
+          </Route>
+          <Route path="*">
+            <NotFound/>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
