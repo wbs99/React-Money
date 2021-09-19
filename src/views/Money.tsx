97c6dfdd6977d32f52b65ba5e9b79ceccfb13b71
wbs <1134954328@qq.com>
2021-09-19 20:24:09 +0800

    完成备注模块

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -25,7 +25,26 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `;
-const NotesSection = styled.section``;
+const NotesSection = styled.section`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  >label{
+    display: flex;
+    align-items: center;
+    >span{
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+    >input{
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
+`;
 const CategorySection = styled.section``;
 const NumberPadSection = styled.section``;
 
@@ -45,7 +64,7 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text" />
+          <input type="text" placeholder='在这里输入备注'/>
         </label>
       </NotesSection>
       <CategorySection>
