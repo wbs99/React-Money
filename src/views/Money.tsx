0b11d500f64361141022f549aadea418a30794f7
wbs <1134954328@qq.com>
2021-09-19 20:59:38 +0800

    完成数字输入区

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import styled from "styled-components";
-import Layout from "../components/Layout";
+import React from 'react';
+import styled from 'styled-components';
+import Layout from '../components/Layout';
 
 const TagsSection = styled.section`
   background-color: #ffffff;
   padding: 12px 16px;
+
   > ul {
     margin: 0 -12px;
+
     > li {
       background-color: #d9d9d9;
       border-radius: 18px;
@@ -16,6 +18,7 @@ const TagsSection = styled.section`
       margin: 8px 12px;
     }
   }
+
   > button {
     background: none;
     border: none;
@@ -29,13 +32,16 @@ const NotesSection = styled.section`
   background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
+
   > label {
     display: flex;
     align-items: center;
+
     > span {
       margin-right: 16px;
       white-space: nowrap;
     }
+
     > input {
       display: block;
       width: 100%;
@@ -47,15 +53,18 @@ const NotesSection = styled.section`
 `;
 const CategorySection = styled.section`
   font-size: 24px;
+
   > ul {
     display: flex;
     background-color: #c4c4c4;
+
     > li {
       width: 50%;
       text-align: center;
       padding: 16px 0;
       position: relative;
-      &.selected::after{
+
+      &.selected::after {
         content: '';
         display: block;
         height: 3px;
@@ -68,7 +77,74 @@ const CategorySection = styled.section`
     }
   }
 `;
-const NumberPadSection = styled.section``;
+const NumberPadSection = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  > .output {
+    background: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+    inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+  }
+
+  > .pad {
+    > button {
+      font-size: 18px;
+      float: left;
+      width: 25%;
+      height: 64px;
+      border: none;
+
+      &.ok {
+        height: 128px;
+        float: right;
+      }
+
+      &.zero {
+        width: 50%;
+      }
+
+      &:nth-child(1) {
+        background: #f2f2f2;
+      }
+
+      &:nth-child(2),
+      &:nth-child(5) {
+        background: #e0e0e0;
+      }
+
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background: #d3d3d3;
+      }
+
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background: #c1c1c1;
+      }
+
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background: #b8b8b8;
+      }
+
+      &:nth-child(12) {
+        background: #9a9a9a;
+      }
+
+      &:nth-child(14) {
+        background: #a9a9a9;
+      }
+    }
+  }
+`;
 
 function Money() {
   return (
@@ -86,18 +162,18 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text" placeholder="在这里输入备注" />
+          <input type="text" placeholder="在这里输入备注"/>
         </label>
       </NotesSection>
       <CategorySection>
         <ul>
-          <li className='selected'>支出</li>
-          <li >收入</li>
+          <li className="selected">支出</li>
+          <li>收入</li>
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>100</div>
-        <div>
+        <div className="output">100</div>
+        <div className="pad clearfix">
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -109,12 +185,13 @@ function Money() {
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
-          <button>0</button>
-          <button>.</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
+          <button className="dot">.</button>
         </div>
       </NumberPadSection>
     </Layout>
   );
 }
+
 export default Money;
